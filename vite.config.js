bda3775f8154619b3d655ccf424d932e88b89059
Xtra3678 <tjcomer7@gmail.com>
2025-03-08 04:00:42 -0500

    github... pls...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/faustdle/',  // Change this to match your repository name
   server: {
     port: 3000,
     open: true,
@@ -14,14 +14,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
+    assetsDir: 'assets'
   }
 });
