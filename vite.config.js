7f93ec667c2c003d74dc07097b1047a837401a5c
Xtra3678 <tjcomer7@gmail.com>
2025-03-08 04:08:11 -0500

    commit...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/faustdle/',  // Change this to match your repository name
+  base: '/faustdle/',
   server: {
     port: 3000,
     open: true,
